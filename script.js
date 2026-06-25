7bfcca07df32201831be9136a31ab5812c8553e5
giulivramento-create <a.livramento@aluno.senai.br>
2026-06-25 11:39:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function converter(){
+    let opcao, valor, total;
+    opcao=parseInt(document.getElementById("txtopcao").value);
+    valor=parseFloat(document.getElementById("txtvalor").value);
+    switch(opcao){
+    case 1:
+        total=valor/5.20;
+        resultado.innerHTML=("Seu valor é: US$" + total.toFixed(2));
+        break;
+    case 2:
+        total=valor/5.91;
+        resultado.innerHTML=("Seu valor é: €" + total.toFixed(2));
+        break;
+        
+    default:
+        resultado.innerHTML=("Opção Inválida.");
+    }
+}
